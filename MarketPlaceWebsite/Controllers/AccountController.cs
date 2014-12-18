08fb290324419237811c1d40a061c1fc3d1e049a
Deserteagle93 <stephenbugi@gmail.com>
2014-12-18 13:54:42 +0100

    Products nearly finished

--- MarketPlaceWebsite/Controllers/AccountController.cs
+++ MarketPlaceWebsite/Controllers/AccountController.cs
@@ -89,7 +89,7 @@ namespace MarketPlaceWebsite.Controllers
                 if (createStatus == MembershipCreateStatus.Success)
                 {
                     FormsService.SignIn(model.UserName, false /* createPersistentCookie */);
-                    return RedirectToAction("Index", "Hoe");
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
